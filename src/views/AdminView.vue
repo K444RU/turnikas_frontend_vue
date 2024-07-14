ffd9b4a5b832e67b4a86e199b2bb78d83fc349a8
olegtrofimov <olegtrofimov96@gmail.com>
2024-07-14 12:44:33 +0300

    TURN | create admin tournament list layout

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -25,7 +25,8 @@
               <div class="find-and-filter-bar">
                 <div class="input-group">
                   <!--Find input to look for a tournament-->
-                  <input type="text" class="find-tournament-input" placeholder="Find a tournament..." aria-label="Find a tournament"
+                  <input type="text" class="find-tournament-input" placeholder="Find a tournament..."
+                         aria-label="Find a tournament"
                          aria-describedby="addon-wrapping">
                 </div>
                 <!--First filter dropdown of tournament Age category-->
@@ -59,9 +60,21 @@
                 </button>
               </div>
 
-              <div class="tournaments-list-body">
+              <div v-for="tournament in tournamentInfoResponse" :key="tournament.id" class="tournaments-list-body" >
                 <div class="tournament-info-box">
-
+                  <div class="tournament-left-info">
+                    <div class="tournament-name-and-type">
+                      <h3>
+                        <a class="tournament-name">{{ tournament.name }}</a>
+                        <span></span>
+                        <span class="tournament-type">{{ getCategoryNameByCategoryCode2(tournament.categoryCode)}}</span>
+                      </h3>
+                    </div>
+                    <div class="tournament-dates">{{ tournament.startDate }} - {{ tournament.endDate }}</div>
+                  </div>
+                  <div class="tournament-right-info">
+                    <button class="tournament-details-button">Details...</button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -83,10 +96,110 @@ export default {
   name: 'adminRoute',
   components: {UserProfileHeader},
   data() {
-    return {}
+    return {
+      tournamentInfoResponse: [],
+      ageCategoryResponse: [],
+      playerAmountResponse:[],
+      cityNameResponse:[],
+      cityNames: {},
+    };
   },
-  computed: {},
-  methods: {}
+  computed: {
+    remainingSlots() {
+      return this.tournamentInfoResponse.amountName - this.registeredTeams.length;
+    },
+  },
+  methods: {
+    getTournamentsInformation() {
+      this.$http.get("/tournament/all")
+          .then(response => {
+            console.log(response.data);
+            this.tournamentInfoResponse = response.data.map(tournament => ({
+              ...tournament,
+              tournamentId: tournament.id
+            }));
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    },
+    getCityName(cityId) {
+      if (this.cityNames[cityId]) {
+        return this.cityNames[cityId];
+      }
+
+      this.$http.get(`/tournament/city/info?cityId=${cityId}`)
+          .then(response => {
+            const cityName = response.data.cityName || 'City Not Found';
+            this.$set(this.cityNames, cityId, cityName);
+          })
+          .catch(error => {
+            console.log(error);
+            this.$set(this.cityNames, cityId, 'City Not Found');
+          });
+
+      return this.cityNames[cityId] || 'Loading...';
+    },
+    navigateToTournamentPage(tournamentId) {
+      console.log("Navigating to tournament page with tournamentId: ", tournamentId);
+      this.$router.push({
+        name: 'tournamentRoute',
+        query: {
+          tournamentId: tournamentId,
+        },
+      });
+    },
+    getAllTournamentCities() {
+      this.$http.get("/tournament/city")
+          .then(response => {
+            this.cityNameResponse = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    getCityNames(cityId){
+      const city = this.cityNameResponse.find(c => c.id === cityId);
+      return city ? city.cityName : 'Unknown';
+    },
+    getAllPlayerAmounts() {
+      this.$http.get("/tournament/player/amount")
+          .then(response => {
+            this.playerAmountResponse = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    getPlayerAmountNames(amountCode){
+      const amount = this.playerAmountResponse.find(a => a.amountCode === amountCode);
+      return amount ? amount.amountName : 'Unknown';
+    },
+    getAgeCategories() {
+      this.$http.get("/team/all/age/category")
+          .then(response => {
+            this.ageCategoryResponse = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    getCategoryNameByCategoryCode2(categoryCode) {
+      const category = this.ageCategoryResponse.find(cat => cat.categoryCode === categoryCode);
+      return category ? category.categoryName : 'Unknown';
+    },
+  },
+  beforeMount() {
+    this.getTournamentsInformation()
+  },
+  mounted() {
+    this.getAgeCategories()
+    this.getAllPlayerAmounts()
+    this.getAllTournamentCities()
+  }
 }
 </script>
 
@@ -106,7 +219,7 @@ export default {
   display: flex;
   flex: 1;
   overflow: hidden;
-  border: 1px solid gray;
+  /*border: 1px solid gray;*/
   margin-top: 100px;
 }
 
@@ -115,7 +228,7 @@ export default {
   width: 2.5%;
   display: flex;
   height: auto;
-  border: 1px solid gray;
+  /*border: 1px solid gray;*/
 }
 
 .admin-view-page-centre-nav {
@@ -125,7 +238,7 @@ export default {
   justify-content: flex-start;
   flex: 1;
   min-height: auto;
-  border: 1px solid gray;
+  /*border: 1px solid gray;*/
   padding: 0 40px;
 }
 
@@ -190,16 +303,86 @@ export default {
 }
 
 .tournament-info-box {
-  width: 100%;
-  height: 100px;
+  width: 100% !important;
+  height: 70px;
   border-top: 1px solid gray;
-  border-bottom: 1px solid gray;
+  justify-content: space-between !important;
+  display: flex;
+  color: whitesmoke;
+}
+
+.tournament-name-and-type {
+  display: inline-block !important;
+  color: whitesmoke;
+  box-sizing: border-box;
+}
+
+.tournament-name {
+  word-break: break-all !important;
+  color: #4493f8;
+  text-decoration: none;
+  background-color: transparent;
   cursor: pointer;
 }
 
-.tournament-info-box:hover {
-  background-color: #333333;
-  cursor: pointer;
+h3 {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+a {
+  color: #4493f8;
+  text-decoration: none;
+  background-color: transparent;
+}
+
+.tournament-type {
+  border: 1px solid gray;
+  border-radius: 2em;
+  display: inline-block;
+  padding: 0 7px;
+  line-height: 18px;
+  font-size: small;
+  margin-left: 5px;
+}
+
+.tournament-dates {
+  color: #8d96a0 !important;
+  font-size: 13px;
+}
+
+.tournament-left-info {
+  /*border: 1px solid gray;*/
+  display: inline-block;
+  width: 83%;
+  box-sizing: border-box;
+}
+
+.tournament-right-info {
+  /*border: 1px solid gray;*/
+  display: flex;
+  align-items: flex-end !important;
+  justify-content: space-around !important;
+  flex-direction: column !important;
+  width: 17%;
+  box-sizing: border-box;
+}
+
+.tournament-details-button {
+  min-width: 100px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #272e36;
+  border: 1px solid gray;
+  box-shadow: none;
+  color: #FFFFFF;
+  font-size: 14px;
+}
+
+.tournament-details-button:hover {
+  background-color: rgba(255, 255, 255, 0.14);
 }
 
 @media (max-width: 768px) {
@@ -209,7 +392,7 @@ export default {
   }
 
   .admin-view-page-body {
-    margin-top: 120px;
+    margin-top: 150px;
     flex-direction: column;
     align-items: center;
   }
@@ -217,6 +400,7 @@ export default {
   .find-and-filter-bar {
     flex-wrap: wrap;
     margin-top: 10px;
+    margin-bottom: 160px;
   }
 
   .find-tournament-input,
@@ -228,7 +412,19 @@ export default {
   }
 
   .tournaments-list-body {
-    margin-top: 150px;
+    margin-top: 50px;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .tournament-right-info {
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+
+  .tournament-details-button {
+    width: 100%;
+    text-align: center;
   }
 }
 </style>
